b0f0a4d45db379d5aeecb8fcfee681b7a1e2f192
Enesa-H <eh73803@ubt-uni.net>
2026-05-28 17:16:32 +0200

    Update server.js

--- cms-backend/server.js
+++ cms-backend/server.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
   res.json({ message: 'CMS Blog API po punon!' });
 });
