08a5cca1162455faa372fc92b093ed7a04478d30
Sela-P <sp73621@ubt-uni.net>
2026-05-30 19:42:53 +0200

    Update App.jsx

--- cms-frontend/src/App.jsx
+++ cms-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Posts from './pages/Posts';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/posts" element={<Posts />} />
       </Routes>
     </BrowserRouter>
   );
