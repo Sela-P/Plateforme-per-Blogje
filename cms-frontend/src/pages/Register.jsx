3254ac8fb0a4988db63f7bf89163e2b461995631
Enesa-H <eh73803@ubt-uni.net>
2026-05-31 21:18:57 +0200

    Update Register.jsx

--- cms-frontend/src/pages/Register.jsx
+++ cms-frontend/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-input';
+import { useNavigate } from "react-router-dom";
 
 function Register() {
   const [form, setForm] = useState({ emri: '', email: '', password: '' });
