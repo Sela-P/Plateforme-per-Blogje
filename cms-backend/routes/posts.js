5d864faad599936e844d9da5bc977cf92fc6ae83
Sela-P <sp73621@ubt-uni.net>
2026-06-01 18:16:54 +0200

    Update posts.js

--- cms-backend/routes/posts.js
+++ cms-backend/routes/posts.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middleware/authMiddleware');
-const { getPosts, getPost, createPost, updatePost, deletePost } = require('../controllers/postController');
+const { getPosts, getPost, createPost, updatePost, deletePost, searchPosts, getPostsByCategory, getPostsByTag } = require('../controllers/postController');
 
 router.get('/', getPosts);
 router.get('/:id', getPost);
 router.post('/', verifyToken, createPost);
 router.put('/:id', verifyToken, updatePost);
 router.delete('/:id', verifyToken, deletePost);
+router.get('/search', searchPosts);
+router.get('/category/:id', getPostsByCategory);
+router.get('/tag/:id', getPostsByTag);
 
 module.exports = router;
